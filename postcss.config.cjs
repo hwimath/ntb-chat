1044abc8cdc99859e99d5f684cb51bf4965e8a11
hwimath <jungrib0304@gmail.com>
2025-08-09 22:59:22 +0900

    fix: Use .cjs extension for PostCSS config

---
+++ postcss.config.cjs
@@ -0,0 +1,7 @@
+// 가장 안정적인 표준 CommonJS 문법으로 설정합니다.
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-// module.exports를 export default로 변경하여 최신 자바스크립트 문법(ESM)을 따르도록 수정합니다.
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
-
-export default config;
